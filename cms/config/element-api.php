c73cdfc917d5b23590adc5141787d821b02baa7f
DaveP <dave@wearecrystal.uk>
2018-04-29 00:40:28 +0100

    ElementAPI transformer

--- cms/config/element-api.php
+++ cms/config/element-api.php
@@ -4,32 +4,37 @@ use craft\elements\Entry;
 
 return [
   'endpoints' => [
-    'work' => [
-      'elementType' => Entry::class,
-      'criteria' => ['section' => 'work'],
-      'transformer' => function(Entry $entry) {
-        return [
-          'title' => $entry->title,
-          'slug' => $entry->slug,
-          'image' => '',
-          'url' => $entry->url,
-          'intro' => $entry->intro,
-          'link' => $entry->external,
-          'clients' => array_map(function($client) {
-            return [
-              'title' => $client->title,
-              'slug' => $client->slug,
-            ];
-          }, $entry->clients->all()),
-          'topics' => array_map(function($topic) {
-            return [
-              'title' => $topic->title,
-              'slug' => $topic->slug,
-            ];
-          }, $entry->topics->all()),
-        ];
-      },
-    ],
+    'work' => function() {
+      \Craft::$app->response->headers->set('Access-Control-Allow-Origin', '*');
+
+      return [
+        'elementType' => Entry::class,
+        'criteria' => ['section' => 'work'],
+        'transformer' => function(Entry $entry) {
+          return [
+            'title' => $entry->title,
+            'slug' => $entry->slug,
+            'date' => $entry->postDate,
+            'image' => '',
+            'url' => $entry->url,
+            'intro' => $entry->intro,
+            'link' => $entry->external,
+            'clients' => array_map(function($client) {
+              return [
+                'title' => $client->title,
+                'slug' => $client->slug,
+              ];
+            }, $entry->clients->all()),
+            'topics' => array_map(function($topic) {
+              return [
+                'title' => $topic->title,
+                'slug' => $topic->slug,
+              ];
+            }, $entry->topics->all()),
+          ];
+        },
+      ];
+    },
     // 'news/<entryId:\d+>.json' => function($entryId) {
     //     return [
     //         'elementType' => Entry::class,
