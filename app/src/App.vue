54c65e41d5499d535f5122d81bec7a4a4eca5184
DaveP <dave@wearecrystal.uk>
2018-04-29 17:51:03 +0100

    Styles

--- app/src/App.vue
+++ app/src/App.vue
@@ -22,12 +22,29 @@ export default {
 @import '../node_modules/bootstrap/scss/bootstrap.scss';
 
 #app {
-  font-family: Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
+  font-family: sans-serif;
   font-size: 16px;
   font-weight: 300;
+  line-height: 1.25;
 }
 
 section {
   padding: 60px;
+}
+
+.under {
+  position: relative;
+  margin-bottom: 15px;
+
+  &:after {
+    content: '';
+    position: absolute;
+    width: 60px;
+    height: 2px;
+    background-color: purple;
+    bottom: 0;
+    left: 2px;
+  }
 }
 </style>
