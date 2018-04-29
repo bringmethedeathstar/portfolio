472889c21b43641ff8bd41ad67e338ce66fc93f1
DaveP <dave@wearecrystal.uk>
2018-04-29 17:01:29 +0100

    Added intro API route

--- cms/config/element-api.php
+++ cms/config/element-api.php
@@ -4,10 +4,37 @@ use craft\elements\Entry;
 
 return [
   'endpoints' => [
+    'intro' => function() {
+      \Craft::$app->response->headers->set('Access-Control-Allow-Origin', '*');
+
+      return [
+        'elementType' => Entry::class,
+        'criteria' => ['id' => 26],
+        'one' => true,
+        'transformer' => function(Entry $entry) {
+          $hero = $entry->main->one();
+          $image = $entry->profile->one();
+
+          return [
+            'title' => $entry->title,
+            'slug' => $entry->slug,
+            'text' => $entry->simple,
+            'hero' => [
+              'title' => $hero->title ?? '',
+              'url' => $hero->url ?? '',
+            ],
+            'profile' => [
+              'title' => $image->title ?? '',
+              'url' => $image->url ?? '',
+            ],
+          ];
+        },
+      ];
+    },
+
     'work' => function() {
       \Craft::$app->response->headers->set('Access-Control-Allow-Origin', '*');
 
-      
       return [
         'elementType' => Entry::class,
         'criteria' => ['section' => 'work'],
@@ -18,13 +45,13 @@ return [
             'title' => $entry->title,
             'slug' => $entry->slug,
             'date' => $entry->postDate,
+            'url' => $entry->url,
+            'intro' => $entry->intro,
+            'link' => $entry->external,
             'image' => [
               'title' => $image->title ?? '',
               'url' => $image->url ?? '',
             ],
-            'url' => $entry->url,
-            'intro' => $entry->intro,
-            'link' => $entry->external,
             'clients' => array_map(function($client) {
               return [
                 'title' => $client->title,
