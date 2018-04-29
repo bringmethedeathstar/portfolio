a860869bd132e8cd6430ca7178dfd7e27d958bbc
DaveP <dave@wearecrystal.uk>
2018-04-29 17:01:49 +0100

    Card(s)

---
+++ app/src/components/cards/Cards.vue
@@ -0,0 +1,61 @@
+<script>
+import api from '@/resources/base';
+import Card from '@/components/cards/Card';
+
+export default {
+  components: { Card },
+
+  data() {
+    return {
+      list: [
+        {
+          title: '',
+          date: {},
+          slug: '',
+          image: {},
+          url: '',
+          intro: '',
+          link: '',
+          clients: [
+            {
+              title: '',
+              slug: '',
+            },
+          ],
+          topics: [
+            {
+              title: '',
+              slug: '',
+            },
+          ],
+        },
+      ],
+    };
+  },
+
+  methods: {
+    fetch() {
+      api.get('/work').then(work => {
+        this.list = work.data.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetch();
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4" v-for="entry in list" :key="entry.title">
+        <Card :card="entry" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
