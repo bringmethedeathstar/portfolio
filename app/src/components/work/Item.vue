65a34f9b12892bd942b5c21dbecb5afec8a9372c
DaveP <dave@wearecrystal.uk>
2018-05-04 23:57:03 +0100

    shitty layout

--- app/src/components/work/Item.vue
+++ app/src/components/work/Item.vue
@@ -34,7 +34,7 @@ export default {
   methods: {
     fetch() {
       api.get(`/work/${this.$route.params.slug}`).then(work => {
-        this.item = work.data.data;
+        this.item = work.data;
       });
     },
   },
@@ -46,12 +46,66 @@ export default {
 </script>
 
 <template>
-  <section>
-    <div class="container">
-      <h1 class="alive">ayy</h1>
-    </div>
-  </section>
+  <div>
+    <section class="hero" :style="`background-image: url('${item.image.url}')`">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-sm-8">
+            <h2>{{ item.title }}</h2>
+
+            <div v-html="item.intro"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-sm-10">
+            <div class="project">
+              <pre>{{ item }}</pre>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.hero {
+  height: 525px;
+  position: relative;
+  color: white;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  font-weight: normal;
+  padding-top: 20vh;
+
+  &:before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: rgba(#444, .6)
+  }
+}
+
+h2 {
+  font-weight: normal;
+}
+
+section {
+  background-color: #f5f5f5;
+}
+
+.project {
+  padding: 30px;
+  background-color: #f5f5f5;
+  margin-top: -200px;
+}
 </style>
