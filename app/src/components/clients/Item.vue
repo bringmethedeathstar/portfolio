42921b625b54083154df4390a48db604ad9cb85c
DaveP <dave@wearecrystal.uk>
2018-05-28 11:57:01 +0100

    Work rename

---
+++ app/src/components/clients/Item.vue
@@ -0,0 +1,89 @@
+<script>
+import api from '@/resources/base';
+import Date from '@/components/parts/Date';
+import Matrix from '@/components/matrix/Blocks';
+
+export default {
+  components: { Date, Matrix },
+
+  data() {
+    return {
+      item: {
+        date: {},
+        client: { slug: '' },
+        topics: [],
+        article: [],
+      },
+    };
+  },
+
+  methods: {
+    fetch() {
+      api.get(`/work/${this.$route.params.slug}`).then(work => {
+        this.item = work.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetch();
+  },
+};
+</script>
+
+<template>
+  <div>
+    <section>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6 text-center">
+            <router-link :to="item.client.slug" class="client">
+              <img :src="item.client.icon" :alt="`${item.client.title} Brand`">
+              {{ item.client.title }}
+            </router-link>
+
+            <h1>{{ item.title }}</h1>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-lg-6 text-center">
+            <p class="date text-center under"><Date :date="item.date.date" /></p>
+
+            <p v-if="item.intro">{{ item.intro }}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <Matrix :blocks="item.article" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+section {
+  padding-bottom: 0;
+}
+
+.client {
+  display: block;
+  font-style: 14px;
+  margin-bottom: 30px;
+
+  img {
+    display: block;
+    margin: 0 auto;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-bottom: 10px;
+  }
+}
+
+.date {
+  font-style: italic;
+  display: block;
+  font-size: 14px;
+  padding-bottom: 30px;
+}
+</style>
---
+++ app/src/components/work/Index.vue
@@ -0,0 +1,46 @@
+<script>
+import api from '@/resources/base';
+import Card from '@/components/parts/Card';
+
+export default {
+  components: { Card },
+
+  data() {
+    return {
+      list: [],
+    };
+  },
+
+  methods: {
+    fetch() {
+      api.get('/work').then(work => {
+        this.list = work.data.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetch();
+  },
+};
+</script>
+
+<template>
+  <section>
+    <div class="container">
+      <h1>My Work</h1>
+
+      <div class="row">
+        <div class="col-sm-6 col-md-4" v-for="entry in list" :key="entry.title">
+          <Card :card="entry" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section {
+  padding-top: 30px;
+}
+</style>
