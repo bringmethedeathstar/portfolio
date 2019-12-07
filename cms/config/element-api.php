ba5b33afc5ea2cf5e3b86ecae43e2b15d34d762e
DaveP <dave@wearecrystal.uk>
2019-12-07 18:03:25 +0000

    Use ratchet for work items

--- cms/config/element-api.php
+++ cms/config/element-api.php
@@ -71,64 +71,17 @@ return [
         'criteria' => ['section' => 'work', 'slug' => $slug],
         'one' => true,
         'transformer' => function(Entry $entry) {
-          if ($asset = $entry->main->one()) {
-            $image = [ 'title' => $asset->title, 'url' => $asset->getUrl('work') ];
-          }
+          $ratchet = new Fields([
+            'fields' => ['main' => ['transforms' => ['featured']]],
+            '^exclude' => ['basic', 'topics'],
+            '^include' => [
+              'date' => function($entry) {
+                return $entry->postDate;
+              },
+            ],
+          ]);
 
-          return [
-            'title' => $entry->title,
-            'slug' => $entry->slug,
-            'date' => $entry->postDate,
-            'intro' => $entry->intro,
-            'link' => $entry->external,
-            'image' => $image ?? '',
-
-            'clients' => array_map(function($client) {
-              $icon = $client->icon->one();
-
-              return [
-                'id' => $client->id,
-                'title' => $client->title,
-                'slug' => $client->slug,
-                'iconStyle' => $client->iconStyle->value,
-                'show' => $client->show,
-                'icon' => $icon ? $icon->getUrl('clientIcon') : '',
-              ];
-            }, $entry->client->all()),
-
-            'topics' => array_map(function($topic) {
-              return [
-                'title' => $topic->title,
-                'slug' => $topic->slug,
-              ];
-            }, $entry->topics->all()),
-
-            'article' => array_map(function($article) {
-              $image = [];
-
-              if ($article->type->handle === 'intro') {
-                if ($img = $article->image->one()) {
-                  $image = [ 'title' => $img->title, 'url' => $img->getUrl($img->style->value) ];
-                }
-              } else {
-               $image = array_map(function($img) {
-                  return [
-                    'title' => $img->title,
-                    'url' => $img->getUrl($img->style->value),
-                    'layout' => $img->imgWidth->value
-                  ];
-                }, $article->image->all());
-              }
-
-              return [
-                'type' => $article->type->handle,
-                'text' => $article->text,
-                'id' => $article->id,
-                'under' => $article->under,
-                'image' => $image,
-              ];
-            }, $entry->basic->all()),
-          ];
+          return $ratchet->run($entry);
         },
       ];
     },
