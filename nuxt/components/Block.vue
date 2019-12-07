63eebd6383fe1ab717bb37203847643ad04b21b5
DaveP <dave@wearecrystal.uk>
2019-12-07 17:23:55 +0000

    Use block component for home content

---
+++ nuxt/components/Block.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: ['title', 'text', 'image', 'flip', 'link'],
+};
+</script>
+
+<template>
+  <div class="flex -mx-8 push" :class="{ 'flex-row-reverse': flip }">
+    <div class="w-7/12 px-8">
+      <img class="w-full profile" :src="image" :alt="title" />
+    </div>
+
+    <div class="w-5/12 px-8 pt-12">
+      <h1 class="mb-8">{{ title }}</h1>
+      <div class="mb-16" v-html="text"></div>
+
+      <n-link class="text-primary" :to="link.to">{{ link.title }} &rarr;</n-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,5 +1,8 @@
 <script>
+import Block from '~/components/Block';
+
 export default {
+  components: { Block },
   async asyncData({ $axios, error }) {
     try {
       const page = await $axios.$get('intro');
@@ -16,37 +19,24 @@ export default {
 
 <template>
   <div class="container">
-    <div class="flex -mx-8 push">
-      <div class="w-5/12 px-8 pt-12">
-        <h1 class="mb-8">Leah Walker</h1>
-        <div class="mb-16" v-html="page.simple"></div>
-
-        <n-link class="text-primary" to="/contact">Hire Me &rarr;</n-link>
-      </div>
-
-      <div class="w-7/12 px-8">
-        <img class="w-full profile" :src="page.main.profile" alt />
-      </div>
-    </div>
+    <Block
+      flip="true"
+      title="Leah Walker"
+      :text="page.simple"
+      :image="page.main.profile"
+      :link="{ to: '/contact', title: 'Hire Me' }"
+    />
 
     <h2>My Work</h2>
-    <div
-      class="flex -mx-8 push"
-      :class="{ 'flex-row-reverse': i % 2 }"
+    <Block
+      :flip="i % 2"
+      :title="work.title"
+      :text="work.intro"
+      :image="work.main.featured"
+      :link="{ to: `/work/${work.slug}`, title: 'Read Story' }"
       v-for="(work, i) in page.featuredWork"
       :key="work.id"
-    >
-      <div class="w-7/12 px-8">
-        <img class="w-full" :src="work.main.featured" alt />
-      </div>
-
-      <div class="w-5/12 px-8 pt-12">
-        <p class="mb-8 text-3xl font-bold leading-tight">{{ work.title }}</p>
-        <p class="mb-16">{{ work.intro }}</p>
-
-        <n-link class="text-primary" :to="`/work/${work.slug}`">Read Story &rarr;</n-link>
-      </div>
-    </div>
+    />
 
     <div class="push text-center">
       <n-link class="text-primary" to="/work">See my portfolio &rarr;</n-link>
