91ffcacfbe4cc2f33d9ee1b6b751be6e94019209
DaveP <dave@wearecrystal.uk>
2019-12-07 17:09:05 +0000

    Tidy

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -4,14 +4,14 @@ export default {
   mode: 'universal',
 
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Creative Copywriter 🖋️ Leah Walker',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: '',
       },
     ],
     link: [
@@ -24,7 +24,7 @@ export default {
     ],
   },
 
-  loading: { color: '#fff' },
+  loading: { color: '#e53e3e' },
 
   css: [],
 
