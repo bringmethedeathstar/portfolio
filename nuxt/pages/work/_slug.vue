c89128b9e7ba531b8025d1ed519ed3c8672102fb
DaveP <dave@wearecrystal.uk>
2019-12-07 18:03:42 +0000

    Started work item template

---
+++ nuxt/pages/work/_slug.vue
@@ -0,0 +1,30 @@
+<script>
+import Block from '~/components/Block';
+
+export default {
+  components: { Block },
+
+  async asyncData({ $axios, error, route }) {
+    try {
+      const page = await $axios.$get(`/work/${route.params.slug}`);
+
+      return { page };
+    } catch (e) {
+      error(e);
+    }
+  },
+
+  data: () => ({ page: [] }),
+};
+</script>
+
+<template>
+  <div class="container">
+    <Block :title="page.title" :text="page.intro" :image="page.main.featured" />
+
+    <pre>{{ page }}</pre>
+  </div>
+</template>
+
+<style scoped>
+</style>
