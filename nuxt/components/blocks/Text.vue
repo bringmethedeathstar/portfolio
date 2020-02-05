3d78053f24b30442696bdf6897ad3e7804f9e22d
Dave Phillips <hello@davep.co>
2020-02-05 22:33:44 +0000

    Added matrix blocks

---
+++ nuxt/components/blocks/Text.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  props: ['block'],
+};
+</script>
+
+<template>
+  <div class="pr-16">
+    <h2>{{ block.label }}</h2>
+    <div class="stack" v-html="block.text"></div>
+  </div>
+</template>
+
+<style scoped></style>
